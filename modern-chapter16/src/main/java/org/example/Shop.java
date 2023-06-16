e5d5eb702457e5335657efc20593413b0422ee5c
kyupid <iamscvv@gmail.com>
2023-06-17 08:42:27 +0900

    16.3 CompletableFuture 개선

--- modern-chapter16/src/main/java/org/example/Shop.java
+++ modern-chapter16/src/main/java/org/example/Shop.java
@@ -3,6 +3,9 @@ package org.example;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.stream.Collectors;
 
 public class Shop {
@@ -56,9 +59,27 @@ public class Shop {
                 .collect(Collectors.toList());
     }
 
+    private static List<String> findPricesV4(List<Shop> shops, String product) {
+        final Executor executor = Executors.newFixedThreadPool(Math.min(shops.size(), 100), new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r);
+                t.setDaemon(true); // 프로그램 종료를 방해하지 않는 데몬 스레드를 사용한다.
+                return t;
+            }
+        });
+
+        List<CompletableFuture<String>> priceFutures = shops.stream()
+                .map(shop -> CompletableFuture.supplyAsync(() -> shop.getName() + " price is " + shop.getPrice(product), executor))
+                .collect(Collectors.toList());
+        return priceFutures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+    }
+
     public static void test(List<Shop> shops) {
         long start = System.nanoTime();
-        System.out.println(findPricesV3(shops, "myPhone27S"));
+        System.out.println(findPricesV4(shops, "myPhone27S"));
         long duration = (System.nanoTime() - start) / 1_000_000;
         System.out.println("Done in " + duration + " msecs");
     }
