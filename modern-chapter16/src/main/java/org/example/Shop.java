beaf413c4034d5b1880e9ab9a79fc77fc91e71e6
kyupid <iamscvv@gmail.com>
2023-06-17 07:57:26 +0900

    16.3 shop 에서 가격찾는 코드

---
+++ modern-chapter16/src/main/java/org/example/Shop.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Shop {
+    private String name;
+    private double price;
+    private Random random = new Random();
+
+    public Shop(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice(String product) {
+        return calculatePrice(product);
+    }
+
+    private double calculatePrice(String product) {
+        delay();
+        return random.nextDouble() * product.charAt(0) + product.charAt(1);
+    }
+
+    public static void delay() {
+        try {
+            Thread.sleep(1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private static List<String> findPrices(List<Shop> shops, String product) {
+        return shops.stream()
+                .map(shop -> String.format("%s price is %.2f", shop.getName(), shop.getPrice(product)))
+                .collect(Collectors.toList());
+    }
+
+    public static void test(List<Shop> shops) {
+        long start = System.nanoTime();
+        System.out.println(findPrices(shops, "myPhone27S"));
+        long duration = (System.nanoTime() - start) / 1_000_000;
+        System.out.println("Done in " + duration + " msecs");
+    }
+}
