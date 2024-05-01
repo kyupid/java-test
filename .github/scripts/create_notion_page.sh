cf1121d5116cd3cd3268cacf427818a3cb4448b5
kyupid <iamscvv@gmail.com>
2024-05-01 14:24:46 +0900

    JIRA-123 @test @account 노션페이지 생성시 모듈 multi select 한개이면 쉼표 없도록 수정
    - 테스트용

--- .github/scripts/create_notion_page.sh
+++ .github/scripts/create_notion_page.sh
@@ -32,7 +32,7 @@ json_payload=$(cat <<EOF
     },
     "모듈": {
       "multi_select": [
-        $(echo $module_names | sed 's/\([^ ]*\)/{\"name\":"\1\"}/g' | tr '\n' ',')
+        $(echo $module_names | sed 's/\([^ ]*\)/{\"name\":"\1\"}/g' | tr '\n' ',' | sed 's/,$//')
       ]
     },
     "Jira": {
