7c3dba5f293253e89a17b288c7fb908d3c530042
Yeonwoo Kim <kywoopid@gmail.com>
2024-05-12 01:02:13 +0900

    DB-123 파이썬으로 변경
    ㅋㅋ

--- .github/scripts/main.py
+++ .github/scripts/main.py
@@ -22,7 +22,7 @@ def main():
             commit_title = re.sub(r'([A-Z]+-[0-9]+|@[a-zA-Z]+)', '', commit_title).strip()
             commit_summary = os.popen(f"git log -1 --pretty=%b {commit}").read()
 
-            version = os.popen("python ./github/scripts/read_version.py").read().strip()
+            version = os.popen("python .github/scripts/read_version.py").read().strip()
 
             if ticket_number or module_names:
                 create_notion_page(ticket_number, module_names, commit_title, commit_summary, version)
